35a7801a1dd0018daf4c451a4cda178f3d4030f6
Documenter.jl <documenter@juliadocs.github.io>
2024-09-23 20:47:31 +0000

    build based on be6b222

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Blast.jl","page":"Home","title":"Blast.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Hello, welcome to Blast.jl documentation! ","category":"page"},{"location":"#Authors","page":"Home","title":"Authors","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Sofia Chiarenza, PhD student at Waterloo Centre for Astrophysics.","category":"page"}]
+}
