4240cd3f9de89f611598a91a63e6360ba08b2c45
Sofia Chiarenza <sofia.chiarenza@studenti.unimi.it>
2024-10-01 16:29:37 -0400

    Added check for compute_T

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,8 @@ using Test
 using Blast
 
 using FastTransforms
+using FastChebInterp
+using NPZ
 
 @testset "Matrix product test" begin
     i = 3
@@ -49,4 +51,69 @@ end
     @test isapprox(T_check[1, :], T_blast[1,:])
     @test isapprox(T_check[2, :], T_blast[2,:])
     @test isapprox(T_check[3, :], T_blast[3,:])
+end
+
+# downloading files LL
+run(`wget https://zenodo.org/api/records/13870111/files-archive`)
+run(`mv files-archive T_tilde_LL_check.tar.xz`)
+run(`mkdir T_tilde_LL_check`)
+run(`tar xvf T_tilde_LL_check.tar.xz -C T_tilde_LL_check`)
+run(`rm T_tilde_LL_check.tar.xz`)
+
+#CL
+run(`wget https://zenodo.org/api/records/13870094/files-archive`)
+run(`mv files-archive T_tilde_CL_check.tar.xz`)
+run(`mkdir T_tilde_CL_check`)
+run(`tar xvf T_tilde_CL_check.tar.xz -C T_tilde_CL_check`)
+run(`rm T_tilde_CL_check.tar.xz`)
+
+#CC
+run(`wget https://zenodo.org/api/records/13870029/files-archive`)
+run(`mv files-archive T_tilde_CC_check.tar.xz`)
+run(`mkdir T_tilde_CC_check`)
+run(`tar xvf T_tilde_CC_check.tar.xz -C T_tilde_CC_check`)
+run(`rm T_tilde_CC_check.tar.xz`)
+
+T_CC_check = zeros(3,96,48,120)
+T_CC_check[1,:,:,:] = npzread("T_tilde_CC_check/T_tilde_l_2.0.npy")[:,:,:,1:120]
+T_CC_check[2,:,:,:] = npzread("T_tilde_CC_check/T_tilde_l_97.1.npy")[:,:,:,1:120]
+T_CC_check[3,:,:,:] = npzread("T_tilde_CC_check/T_tilde_l_211.6.npy")[:,:,:,1:120]
+
+T_CL_check = zeros(3,96,48,120)
+T_CL_check[1,:,:,:] = npzread("T_tilde_CL_check/T_tilde_l_2.0.npy")[:,:,:,1:120]
+T_CL_check[2,:,:,:] = npzread("T_tilde_CL_check/T_tilde_l_97.1.npy")[:,:,:,1:120]
+T_CL_check[3,:,:,:] = npzread("T_tilde_CL_check/T_tilde_l_211.6.npy")[:,:,:,1:120]
+
+T_LL_check = zeros(3,96,48,120)
+T_LL_check[1,:,:,:] = npzread("T_tilde_LL_check/T_tilde_l_2.0.npy")[:,:,:,1:120]
+T_LL_check[2,:,:,:] = npzread("T_tilde_LL_check/T_tilde_l_97.1.npy")[:,:,:,1:120]
+T_LL_check[3,:,:,:] = npzread("T_tilde_LL_check/T_tilde_l_211.6.npy")[:,:,:,1:120]
+
+run(`rm -r T_tilde_CC_check`)
+run(`rm -r T_tilde_CL_check`)
+run(`rm -r T_tilde_LL_check`)
+
+
+@testset "T tilde validation" begin
+    ℓs = [2.0, 97.07777459, 211.63514264]
+    T_CC_blast = zeros(3,96,48,120)
+    T_CL_blast = zeros(3,96,48,120)
+    T_LL_blast = zeros(3,96,48,120)
+
+    chi = LinRange(26,7000,96)
+    R = FastChebInterp.chebpoints(96, -1, 1)
+    R = reverse(R[R.>0])
+    kmax = 200/13 
+    kmin = 2.5/7000
+
+    for (il, l) in enumerate(ℓs)
+        println("Performing integration for ℓ=$l...\n")
+        T_CC_blast[il,:,:,:] = Blast.compute_T̃(l, chi, R, kmin, kmax, 2)
+        T_CL_blast[il,:,:,:] = Blast.compute_T̃(l, chi, R, kmin, kmax, 0)
+        T_LL_blast[il,:,:,:] = Blast.compute_T̃(l, chi, R, kmin, kmax, -2)
+    end
+
+    @test isapprox(T_CC_check, T_CC_blast)
+    @test isapprox(T_CL_check, T_CL_blast)
+    @test isapprox(T_LL_check, T_LL_blast)
 end
