1d02cb1e576d1dd07a3e3eb6e91315aae3838fb1
Sofia Chiarenza <sofia.chiarenza@studenti.unimi.it>
2024-10-03 14:06:29 -0400

    Updated function name

--- src/projected_matter.jl
+++ src/projected_matter.jl
@@ -39,7 +39,7 @@ end
     Bessel_Cheb_eval( ℓ::Number, kmin::Number, kmax::Number, χ::AbstractArray, n_cheb::Int, N::Number)
 Return the Chebyshev polynomials up to order 'n_cheb+1' and the Bessel function of order 'ℓ' evaluated on the grid of 'N' Chebyshev points in the interval ['kmin', 'kmax'] and on the specified 'χ' points. 
 """
-function Bessel_Cheb_eval( ℓ::Number, kmin::Number, kmax::Number, χ::AbstractArray, n_cheb::Int, N::Number)
+function bessel_cheb_eval(ℓ::Number, kmin::Number, kmax::Number, χ::AbstractArray, n_cheb::Int, N::Number)
 
     nχ = length(χ)
     x = get_clencurt_grid(kmin, kmax, N)
