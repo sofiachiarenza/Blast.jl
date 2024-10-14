63f35a28e8890025fd6736e17bffd2229b3552c2
Sofia Chiarenza <sofia.chiarenza@studenti.unimi.it>
2024-10-14 12:19:52 -0400

    Improved T_tilde organization

--- src/Blast.jl
+++ src/Blast.jl
@@ -14,27 +14,17 @@ include("projected_matter.jl")
 include("chebcoefs.jl")
 include("integrals.jl")
 
-function load_precomputed_Ts(folder::String; nχ::Int = 96, nR::Int = 48, n_cheb::Int = 120)
+function load_precomputed_Ts(folder::String)
     ell_vector = npzread(joinpath(folder, "ell_list.npy"))
-    nℓ = length(ell_vector)
-    full_T = zeros(nℓ, nχ, nR, n_cheb)
-    for i in 1:nℓ
-        l_string = string(round(ell_vector[i]; digits=1))
-        filename = joinpath(folder, "T_tilde_l_$l_string.npy")
-        if isfile(filename)
-            full_T[i,:,:,:] = npzread(filename)
-        else
-            throw(LoadError(filename, "File doesn't exist."))
-        end
-    end
-    return full_T
+    full_T = npzread(joinpath(folder, "T_tilde.npy"))
+    return ell_vector, full_T
 end
 
 function __init__()
 
-    global T_tilde_m2 = load_precomputed_Ts(artifact"T_tilde_2")
-    global T_tilde_0 = load_precomputed_Ts(artifact"T_tilde_0")
-    global T_tilde_p2 = load_precomputed_Ts(artifact"T_tilde_-2")
+    global ℓ, T_tilde_m2 = load_precomputed_Ts(artifact"T_tilde_2")
+    global ℓ, T_tilde_0 = load_precomputed_Ts(artifact"T_tilde_0")
+    global ℓ, T_tilde_p2 = load_precomputed_Ts(artifact"T_tilde_-2")
 
 end
 
